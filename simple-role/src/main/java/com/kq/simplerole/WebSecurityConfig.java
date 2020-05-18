a11adbc01f77be6b0799b440f28df5e0ce364ffb
king <kongq1983@qq.com>
2020-05-19 00:48:57 +0800

    spring demo

--- simple-role/src/main/java/com/kq/simplerole/WebSecurityConfig.java
+++ simple-role/src/main/java/com/kq/simplerole/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.kq.simplerole;
 
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
@@ -14,8 +17,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/admin/api/**").hasRole("ADMIN")
-                .antMatchers("/user/api/**").hasRole("USER")
+        // 没有以ROLE_开头  会报403   Access Denied
+        http.authorizeRequests().antMatchers("/admin/api/**").hasRole("ROLE_ADMIN")
+                .antMatchers("/user/api/**").hasRole("ROLE_USER")
                 .antMatchers("/app/api/**").permitAll()
                 .anyRequest().authenticated().and().csrf().disable();
     }
@@ -30,6 +34,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         manager.createUser(User.withUsername("admin").password("123456").roles("USER","ADMIN").build());
 
         return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
